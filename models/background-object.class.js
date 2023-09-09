63163906c7067ad6774fd004d4928ef85375301b
Agend047 <schneiderniklas@gmx.de>
2023-09-09 15:34:51 +0200

    Background-Creation more adaptive

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,6 +1,7 @@
 class Background extends MovableObject {
 
-    constructor(pos_x, imagePath) {
-        super(pos_x, 180, 720, 300,).loadImage(imagePath);
+    constructor(pos_x, height, imagePath) {
+        super(pos_x, 0, 720, height).loadImage(imagePath);
+        this.pos_y = 480 - this.height;
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -3,14 +3,14 @@ class World {
     air = new GameObject(0, 0, 720, 480, 'img/5_background/layers/air.png')
 
     backgroundObjects = [
-        new Background(0, 'img/5_background/layers/3_third_layer/1.png'),
-        new Background(720, 'img/5_background/layers/3_third_layer/2.png'),
+        new Background(0, 400, 'img/5_background/layers/3_third_layer/1.png'),
+        new Background(720, 400, 'img/5_background/layers/3_third_layer/2.png'),
 
-        new Background(0, 'img/5_background/layers/2_second_layer/1.png'),
-        new Background(720, 'img/5_background/layers/2_second_layer/2.png'),
+        new Background(0, 400, 'img/5_background/layers/2_second_layer/1.png'),
+        new Background(720, 400, 'img/5_background/layers/2_second_layer/2.png'),
 
-        new Background(0, 'img/5_background/layers/1_first_layer/1.png'),
-        new Background(720, 'img/5_background/layers/1_first_layer/2.png'),
+        new Background(0, 400, 'img/5_background/layers/1_first_layer/1.png'),
+        new Background(720, 400, 'img/5_background/layers/1_first_layer/2.png'),
     ];
 
     character = new Character(100, 280);
@@ -40,26 +40,13 @@ class World {
 
         this.addToMap(this.air)
 
-        this.clouds.forEach(cloud => {
-            cloud.pos_x = cloud.pos_x - 0.1;
-            this.addToMap(cloud);
-        })
-
-        this.backgroundObjects.forEach(background => {
-            this.addToMap(background);
-        })
-
+        this.addArrayToMap(this.clouds) // cloud.pos_x = cloud.pos_x - 0.1;
+        this.addArrayToMap(this.backgroundObjects)
         this.addToMap(this.character)
-
-        this.enemys.forEach(enemy => {
-            enemy.pos_x = enemy.pos_x - 0.5;
-            this.addToMap(enemy)
-        })
+        this.addArrayToMap(this.enemys)  // enemy.pos_x = enemy.pos_x - 0.5;
 
 
-
-        //calling draw again
-        let self = this;
+        let self = this;  //calling draw again
         setTimeout(function () {
             requestAnimationFrame(function () {
                 self.draw();
@@ -68,11 +55,24 @@ class World {
         )
     }
 
+    /**
+     * Used to include an Array of Elements
+     * @param {Array} objects the Array of elements, we want to add
+     */
+    addArrayToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o)
+        })
+    }
+
+
     /** 
      * @param {Object} mo The MovableObject, we want to draw.
      */
     addToMap(mo) {
         this.ctx.drawImage(mo.img, mo.pos_x, mo.pos_y, mo.width, mo.height)
     }
+
+
 }
 
