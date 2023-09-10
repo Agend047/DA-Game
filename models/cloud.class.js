1e89691a220d8a8d69c899fa49c8549d178140e4
Agend047 <schneiderniklas@gmx.de>
2023-09-10 17:47:10 +0200

    Moving Clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -5,5 +5,13 @@ class Cloud extends MovableObject {
     constructor() {
         let getX = (Math.random() * 420)
         super(getX, 1).loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.cloudMoving()
+    }
+
+    cloudMoving() {
+        setInterval(() => {
+            this.pos_x -= 0.1
+        }, 33)
     }
 }
